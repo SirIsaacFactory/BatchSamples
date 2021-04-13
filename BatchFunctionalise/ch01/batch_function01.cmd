3044e20a78991465d5787a0d80d1983b0cc8543e
Isaac Factory <sir.isaac.factory@gmail.com>
2021-04-13 23:17:35 +0900

    commit

--- BatchFunctionalise/ch01/batch_function01.cmd
+++ BatchFunctionalise/ch01/batch_function01.cmd
@@ -1,5 +1,16 @@
 @echo off
 rem ########################################################
+rem # ファイル名: batch_function01.cmd
+rem # 概要： バッチ関数化サンプル
+rem #-------------------------------------------------------
+rem # Author: Isaac Factory (sir.isaac.factory@icloud.com)
+rem # Repository: https://github.com/SirIsaacFactory/BatchSamples
+rem # Blog: https://isaacfactory.com/
+rem # Date: 2021/04/13
+rem # Code version: v1.00
+rem ########################################################
+
+rem ########################################################
 rem # 現在日時表示関数
 rem ########################################################
 :display_date_time
--- BatchFunctionalise/ch01/batch_function02.cmd
+++ BatchFunctionalise/ch01/batch_function02.cmd
@@ -1,5 +1,16 @@
 @echo off
 rem ########################################################
+rem # ファイル名: batch_function02.cmd
+rem # 概要： バッチ関数化サンプル
+rem #-------------------------------------------------------
+rem # Author: Isaac Factory (sir.isaac.factory@icloud.com)
+rem # Repository: https://github.com/SirIsaacFactory/BatchSamples
+rem # Blog: https://isaacfactory.com/
+rem # Date: 2021/04/13
+rem # Code version: v1.00
+rem ########################################################
+
+rem ########################################################
 rem # メイン関数実行
 rem ########################################################
 call :main
